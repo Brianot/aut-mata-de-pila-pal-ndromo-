e99f19c57c79a8f6bb26df1cb7479341c0029ac0
Brayan Obispo <segunda.bo@gmail.com>
2019-10-24 16:50:24 -0500

    First commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+root = tk.Tk()
+photo = tk.PhotoImage("automata de pila\img\einitial.PNG")
+label = tk.Label(root, image=photo)
+label.pack()
+# Start the program
+root.mainloop()
